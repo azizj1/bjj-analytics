95539d75e5a2523d88a362ddf4df667e70a61ef3
Aziz Javed <azizjaved0@gmail.com>
2018-09-02 00:48:07 -0500

    Setting up build process

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const config = {
+    plugins: {
+        'autoprefixer': {
+            browsers: ['> 1%', 'last 2 versions', 'last 2 major versions', 'not IE 10']
+        }
+    }
+}
+
+module.exports = config;
